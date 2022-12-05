e4ae49871afebe30e90797f8a67660d53af606e5
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2022-12-05 20:01:26 +0200

    SQL1.1

---
+++ java-web/classwork/src/main/java/lesson3/SQLApp1.java
@@ -0,0 +1,22 @@
+package lesson3;
+
+import java.sql.*;
+
+public class SQLApp1 {
+
+  public static void main(String[] args) throws SQLException {
+    Connection conn = DriverManager.getConnection(
+        "jdbc:postgresql://localhost:5432/fs1",
+        "postgres",
+        "pg123456"
+    );
+    PreparedStatement stmt = conn.prepareStatement("select * from account");
+    ResultSet rs = stmt.executeQuery();
+    while (rs.next()) {
+      int id = rs.getInt("id");
+      String name = rs.getString("name");
+      System.out.printf("id: %d, name: %s\n", id, name);
+    }
+    conn.close();
+  }
+}
