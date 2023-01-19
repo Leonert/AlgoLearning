90fbcaefd6dfaee21bfe0392e4f2b14cf0aba5ce
Alexey Rykhalskiy <alexey.rykhalskiy@gmail.com>
2023-01-19 19:40:05 +0200

    algo.2.binary

--- algo/src/main/java/l02/Overflow.java
+++ algo/src/main/java/l02/Overflow.java
@@ -1,28 +1,39 @@
 package l02;
 
-import java.util.function.BiFunction;
-import java.util.function.Function;
-
 public class Overflow {
 
+  /**
+   * 0..255
+   * 0xxxxxxx  0...127 (128)
+   * 1xxxxxxx -1..-128 (128)
+   *
+   * xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+   * 0xxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx - positive
+   * 1xxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx - negative
+   * 00000000 00000000 00000000 00000001 = 1
+   *
+   *  | x | y | & | | | ^ | r
+   *  | - | - | - | - | - | -
+   *  | - | + | - | + | + | +/- don't interest in
+   *  | + | - | - | + | + | +/- don't interest in
+   *  | + | + | + | + | - | +
+   */
   public static boolean isOverflowWhenAdd(int x, int y) {
-    throw new IllegalArgumentException("to implement");
+    int x_sign = x >> 31;
+    int y_sign = y >> 31;
+    if ((x_sign ^ y_sign) == 1) return false; // OK
+    int r_sign = (x + y) >> 31;
+    return r_sign == x_sign; // sign the same
   }
 
-  public static boolean isOverflowWhenSub(int x, int y) {
-    throw new IllegalArgumentException("to implement");
-  }
-
-  public static boolean isOverflowWhenMul(int x, int y) {
-    throw new IllegalArgumentException("to implement");
-  }
-
-  public static boolean isOverflow(int x, int y, BiFunction<Integer, Integer, Integer> f) {
-    throw new IllegalArgumentException("to implement");
+  public static boolean isOverflowWhenAdd2(int x, int y) {
+    int r = x + y;
+    // if and only if both arguments have the opposite sign of the result
+    return ((x ^ r) & (y ^ r)) < 0;
   }
 
   public static void main(String[] args) {
-    isOverflow(Integer.MAX_VALUE, 1, (a, b) -> a + b)
+    isOverflowWhenAdd(Integer.MAX_VALUE, 1);
   }
 
   public static void main1(String[] args) {
